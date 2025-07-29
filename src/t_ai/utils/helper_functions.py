dc018d75c1276c6cf31af2b8c7a613d99b54c497
tristanpadiou <padioutristan@gmail.com>
2025-07-29 12:39:39 -0400

    v0.1.1 quick fix of mcp server

--- src/t_ai/utils/helper_functions.py
+++ src/t_ai/utils/helper_functions.py
@@ -15,13 +15,13 @@ class MCP_server_helper:
         list: A list of MCP servers
     """
     def __init__(self):
-        self.mpc_servers=[]
-    def add_mpc_server(self, type:str, mpc_server_url:str, headers:dict = None, command:str = None, args:list = None, env:dict = None, timeout:int = 60):
+        self.mcp_servers=[]
+    def add_mcp_server(self, type:str, mcp_server_url:None, headers:dict = None, command:str = None, args:list = None, env:dict = None, timeout:int = 60):
         """
         Add an MCP server to the list of MCP servers
         Args:
             type: The type of MCP server to add, can be 'http', 'sse', or 'stdio'
-            mpc_server_url: The URL of the MCP server to add
+            mcp_server_url: The URL of the MCP server to add
             headers: The headers to add to the MCP server
             command: The command to add to the MCP server
             args: The arguments to add to the MCP server
@@ -29,27 +29,27 @@ class MCP_server_helper:
             timeout: The timeout for the MCP server
         Example:
             ```python
-            add_mpc_server(type='http', mpc_server_url='https://mcp.notion.com/mcp', headers=None)
-            add_mpc_server(type='sse', mpc_server_url='https://mcp.notion.com/sse', headers=None)
-            add_mpc_server(type='stdio', command='npx', args=['-y', 'mcp-remote', 'https://mcp.notion.com/mcp'], env=None)
+            add_mcp_server(type='http', mcp_server_url='https://mcp.notion.com/mcp', headers=None)
+            add_mcp_server(type='sse', mcp_server_url='https://mcp.notion.com/sse', headers=None)
+            add_mcp_server(type='stdio', command='npx', args=['-y', 'mcp-remote', 'https://mcp.notion.com/mcp'], env=None)
             ```
         """
         if type == 'http':
             if headers is not None:
-                self.mpc_servers.append(MCPServerStreamableHTTP(url=mpc_server_url, headers=headers, timeout=timeout))
+                self.mcp_servers.append(MCPServerStreamableHTTP(url=mcp_server_url, headers=headers, timeout=timeout))
             else:
-                self.mpc_servers.append(MCPServerStreamableHTTP(url=mpc_server_url, timeout=timeout))
+                self.mcp_servers.append(MCPServerStreamableHTTP(url=mcp_server_url, timeout=timeout))
         elif type == 'sse':
             if headers is not None:
-                self.mpc_servers.append(MCPServerSSE(url=mpc_server_url, headers=headers, timeout=timeout))
+                self.mcp_servers.append(MCPServerSSE(url=mcp_server_url, headers=headers, timeout=timeout))
             else:
-                self.mpc_servers.append(MCPServerSSE(url=mpc_server_url, timeout=timeout))
+                self.mcp_servers.append(MCPServerSSE(url=mcp_server_url, timeout=timeout))
         elif type == 'stdio':
             if env is not None:
-                self.mpc_servers.append(MCPServerStdio(command=command, args=args, env=env, timeout=timeout))
+                self.mcp_servers.append(MCPServerStdio(command=command, args=args, env=env, timeout=timeout))
             else:
-                self.mpc_servers.append(MCPServerStdio(command=command, args=args, timeout=timeout))
+                self.mcp_servers.append(MCPServerStdio(command=command, args=args, timeout=timeout))
         else:
             raise ValueError(f"Invalid type: {type}")
-    def get_mpc_servers(self):
-        return self.mpc_servers
+    def get_mcp_servers(self):
+        return self.mcp_servers
